f632e6549ec58c4c50e000011cb12abe163b18b7
re5pect123 <marko.b.uljarevic@gmail.com>
2023-03-22 21:47:24 +0100

    konekcija sa bazom i docker compose

---
+++ customer/src/main/java/org/amigoscode/domain/Customer.java
@@ -0,0 +1,29 @@
+package org.amigoscode.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Customer {
+
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
+    Integer id;
+    String firstName;
+    String lastName;
+    String email;
+
+}
--- customer/src/main/java/org/amigoscode/service/CustomerServiceImpl.java
+++ customer/src/main/java/org/amigoscode/service/CustomerServiceImpl.java
@@ -1,19 +1,32 @@
 package org.amigoscode.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.amigoscode.domain.Customer;
 import org.amigoscode.domain.CustomerRequest;
 import org.amigoscode.domain.CustomerResponse;
+import org.amigoscode.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService  {
+
+    private final CustomerRepository customerRepository;
 
     @Override
     public String info(CustomerRequest customerRequest) {
 
-        return CustomerResponse.builder()
-                .name(customerRequest.getName() + "ASD")
-                .build().toString();
+        Customer customer = Customer.builder()
+                .firstName(customerRequest.getName())
+                .lastName(customerRequest.getSurname())
+                .build();
+
+
+        customerRepository.save(customer);
+
+
+        return "OK";
     }
 }
